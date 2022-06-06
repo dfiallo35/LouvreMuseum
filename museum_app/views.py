81f28e4983d96e06ee22c5c175caf1bf349feb3d
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-06 00:34:03 -0400

    statics

--- museum_app/views.py
+++ museum_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Artwork
 
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'pages/home_page.html')
+    return render(request, 'pages/home.html')
+
+# def catalog(request):
+#     return render(request, 'pages/catalog.html')
 
 def catalog(request):
-    return render(request, 'pages/catalog.html')
+    artworkslist = Artwork.objects.all()
+    return render(request, 'pages/catalog.html', {'artworkslist':artworkslist})
