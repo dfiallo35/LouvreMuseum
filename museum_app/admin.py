70b3eb857b12ac64eb3eb8f8eb73ea8679f5c513
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-07 12:25:11 -0400

    admin changes

--- museum_app/admin.py
+++ museum_app/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-class ArtworkAd(admin.ModelAdmin):
-    list_display = ('name', 'type', 'author', 'period', 'creation_date', 'economic_valuation', 'entry_date', 'image', 'state')
+# class ArtworkAd(admin.ModelAdmin):
+#     list_display = ('name', 'type', 'author', 'period', 'creation_date', 'economic_valuation', 'entry_date', 'image', 'state')
 
-class StateAd(admin.ModelAdmin):
-    list_display = ('artwork',)
+# class StateAd(admin.ModelAdmin):
+#     list_display = ('artwork',)
 
 class PaintingAd(admin.ModelAdmin):
-    list_display = ('name', 'technique', 'style')
+    list_display = ('name', 'technique', 'style', 'state')
 
 class SculptureAd(admin.ModelAdmin):
-    list_display = ('name', 'material', 'style')
+    list_display = ('name', 'material', 'style', 'state')
 
 class CollaboratingMuseumAd(admin.ModelAdmin):
     list_display = ('name',)
@@ -23,8 +23,8 @@ class RestorationAd(admin.ModelAdmin):
     list_display = ('restoration_type', 'start_date', 'finish_date')
 
 
-admin.site.register(Artwork, ArtworkAd)
-admin.site.register(State, StateAd)
+# admin.site.register(Artwork, ArtworkAd)
+# admin.site.register(State, StateAd)
 admin.site.register(Painting, PaintingAd)
 admin.site.register(Sculpture, SculptureAd)
 admin.site.register(CollaboratingMuseum, CollaboratingMuseumAd)
