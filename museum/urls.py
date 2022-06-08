1f3a99faaea2b47b27097aa7d3eda091520a7809
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-07 22:33:53 -0400

    small fixes

--- museum/urls.py
+++ museum/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('museum_app.urls')),
     path('admin/', admin.site.urls),
+    
 ]
 
 if settings.DEBUG:
--- museum_app/views.py
+++ museum_app/views.py
@@ -6,9 +6,6 @@ from .models import Artwork
 def home(request):
     return render(request, 'pages/home.html')
 
-# def catalog(request):
-#     return render(request, 'pages/catalog.html')
-
 def catalog(request):
     artworkslist = Artwork.objects.all()
     return render(request, 'pages/catalog.html', {'artworkslist':artworkslist})
