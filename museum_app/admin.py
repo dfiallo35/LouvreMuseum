2826a6ebdcbd246dedc7257c7a75bda43d11c070
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-16 14:31:25 -0400

    models admin view update

--- museum_app/admin.py
+++ museum_app/admin.py
@@ -2,48 +2,143 @@ from django.contrib import admin
 from .models import *
 
 class ArtworkAd(admin.ModelAdmin):
-    list_display = ('name', 'author', 'period', 'creation_date', 'entry_date', 'economic_valuation')
-    search_fields = ['name', 'author', 'period']
-    list_filter = ('author', 'period',)
+    list_display = (
+        'name',
+        'author',
+        'period',
+        'creation_date',
+        'entry_date',
+        'economic_valuation',
+        'room',
+        current_state,
+    )
+    search_fields = [
+        'name',
+        'author',
+        'period',
+    ]
+    list_filter = (
+        'author',
+        'period',
+    )
 
-class StateAd(admin.ModelAdmin):
-    list_display = ('artwork',)
-    search_fields =['artwork']
+    
+# @admin.register(State)
+# class StateAd(admin.ModelAdmin):
+#     list_display = (
+#         'artwork',
+#         'date_time',
+#     )
+#     search_fields =[
+#         'artwork',
+#     ]
+#     list_filter=(
+#         'artwork',
+#     )
 
+@admin.register(Painting)
 class PaintingAd(admin.ModelAdmin):
-    list_display = ('name', 'author', 'period', 'creation_date', 'entry_date', 'economic_valuation', 'technique', 'style')
-    search_fields = ['name', 'technique', 'style']
-    list_filter = ('technique', 'style')
+    list_display = (
+        'name',
+        'author',
+        'period',
+        'creation_date',
+        'entry_date',
+        'economic_valuation',
+        'technique',
+        'style',
+        'room',
+    )
+    search_fields = [
+        'name',
+        'technique',
+        'style',
+    ]
+    list_filter = (
+        'technique',
+        'style',
+    )
 
+@admin.register(Sculpture)
 class SculptureAd(admin.ModelAdmin):
-    list_display = ('name', 'author', 'period', 'creation_date', 'entry_date', 'economic_valuation', 'material', 'style')
-    search_fields = ['name', 'material', 'style']
-    list_filter = ('material', 'style')
+    list_display = (
+        'name',
+        'author',
+        'period',
+        'creation_date',
+        'entry_date',
+        'economic_valuation',
+        'material',
+        'style',
+        'room',
+    )
+    search_fields = [
+        'name',
+        'material',
+        'style',
+    ]
+    list_filter = (
+        'material',
+        'style',
+        )
 
+@admin.register(CollaboratingMuseum)
 class CollaboratingMuseumAd(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ['name']
+    list_display = (
+        'name',
+    )
+    search_fields = [
+        'name',
+    ]
 
+@admin.register(Given)
 class GivenAd(admin.ModelAdmin):
-    list_display = ('artwork', 'collaborating_museum', 'given_time', 'date_time', 'amount_received', 'state')
-    search_fields = ['artwork', 'collaborating_museum']
-    list_filter = ('collaborating_museum',)
+    list_display = (
+        'artwork',
+        'collaborating_museum',
+        'given_time',
+        'date_time',
+        'amount_received',
+    )
+    search_fields = [
+        'artwork',
+        'collaborating_museum',
+    ]
+    list_filter = (
+        'collaborating_museum',
+    )
 
+@admin.register(Restoration)
 class RestorationAd(admin.ModelAdmin):
-    list_display = ('artwork', 'restoration_type', 'date_time', 'finish_date', 'state')
-    search_fields = ['artwork', 'restoration_type']
-    list_filter = ('restoration_type',)
+    list_display = (
+        'artwork',
+        'restoration_type',
+        'date_time',
+        'finish_date',
+    )
+    search_fields = [
+        'artwork',
+        'restoration_type',
+    ]
+    list_filter = (
+        'restoration_type',
+    )
 
+@admin.register(Exibition)
 class ExibitionAd(admin.ModelAdmin):
-    list_display = ('artwork', 'date_time', 'state')
-    search_fields = ['artwork']
+        'artwork',
+        'date_time',
+    )
+    search_fields = [
+        'artwork',
+    ]
 
+@admin.register(Room)
+class RoomAd(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = [
+        'name',
+    ]
 
-admin.site.register(Artwork, ArtworkAd)
-admin.site.register(State, StateAd)
-admin.site.register(Painting, PaintingAd)
-admin.site.register(Sculpture, SculptureAd)
-admin.site.register(CollaboratingMuseum, CollaboratingMuseumAd)
-admin.site.register(Given, GivenAd)
-admin.site.register(Restoration, RestorationAd)
-admin.site.register(Exibition, ExibitionAd)
