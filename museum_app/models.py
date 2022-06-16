d575628fb58eed3e5592d91c176d2b102dfb0573
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-16 14:33:30 -0400

    models update

--- museum_app/models.py
+++ museum_app/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.db.models import Model
 from datetime import datetime
 
+class Room(Model):
+    name = models.CharField('Room', max_length=100)
+
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 class Artwork(Model):
     name = models.CharField('Name', max_length=100)
@@ -11,34 +22,58 @@ class Artwork(Model):
     creation_date = models.DateField('Creation Date')
     entry_date = models.DateField('Entry date')
     image = models.ImageField('Image', upload_to="")
-    type = "Artwork"
+    room = models.ForeignKey(Room, verbose_name='Room',
+                            on_delete=models.DO_NOTHING,
+                            related_name='room')
+
+    class Meta:
+        verbose_name = "Artwork"
+        verbose_name_plural = "Artworks"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
 
 class State(Model):
-    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE, verbose_name='Artwork')
-    state = 'Exhibition'
-    date_time = models.DateTimeField('Date and Time')
+    artwork = models.ForeignKey(Artwork, verbose_name='Artwork',
+                                on_delete=models.DO_NOTHING,
+                                related_name='artwork')
+    date_time = models.DateTimeField('Date and Time', auto_now_add=True)
 
-    def __str__(self):
-        return self.state
+    class Meta:
+        verbose_name = "State"
+        verbose_name_plural = "States"
+        get_latest_by = "date_time"
+        ordering = ["date_time"]
+
 
 #Artwork descendants
 class Painting(Artwork):
     technique = models.CharField('Technique', max_length=100)
     style = models.CharField('Style', max_length=100)
-    type = 'Painting'
+
+    class Meta:
+        verbose_name = "Painting"
+        verbose_name_plural = "Paintings"
+        ordering = ["name"]
 
 class Sculpture(Artwork):
     material = models.CharField('Material', max_length=100)
     style = models.CharField('Style', max_length=100)
-    type = 'Sculpture'
+
+    class Meta:
+        verbose_name = "Sculpture"
+        verbose_name_plural = "Sculptures"
+        ordering = ["name"]
 
 
 class CollaboratingMuseum(Model):
     name = models.CharField('Name', max_length=100)
+
+    class Meta:
+        verbose_name = "Collaborating Museum"
+        verbose_name_plural = "Collaborating Museums"
 
     def __str__(self):
         return self.name
@@ -46,16 +81,51 @@ class CollaboratingMuseum(Model):
 
 #State descendants
 class Given(State):
-    collaborating_museum = models.ForeignKey(CollaboratingMuseum, on_delete=models.CASCADE)
+    collaborating_museum = models.ForeignKey(CollaboratingMuseum,
+                                            on_delete=models.DO_NOTHING,
+                                            related_name='collaborating_museum')
     given_time = models.DurationField('Given time')
     amount_received = models.IntegerField('Amount Received')
-    state = 'Given'
+
+    class Meta:
+        verbose_name = "Given"
+        verbose_name_plural = "Givens"
+        get_latest_by = "date_time"
+        ordering = ["date_time"]
+
+    def __str__(self):
+        return 'Given'
 
 class Restoration(State):
     restoration_type = models.CharField('Restoration Type', max_length=100)
-    finish_date = models.DateField('Finish Date')
-    state = 'Restoration'
+    finish_date = models.DateField('Finish Date', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Restauration"
+        verbose_name_plural = "Restaurations"
+        get_latest_by = "date_time"
+        ordering = ["date_time"]
+
+    def __str__(self):
+        return 'Restoration'
     
 
 class Exibition(State):
-    state = 'Exhibition'
+    class Meta:
+        verbose_name = "Exibition"
+        verbose_name_plural = "Exibitions"
+        get_latest_by = "date_time"
+        ordering = ["date_time"]
+
+    def __str__(self):
+        return 'Exibition'
+
+
+
+class Shit(Artwork):
+    class Meta:
+        proxy = True
+        verbose_name = "Shit"
+        verbose_name_plural = "Shits"
+
+
