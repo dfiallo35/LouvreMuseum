b575d0ca362e85b2e5cd38a2a734547aefaef0ad
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-27 01:10:30 -0400

    models changes

--- museum_app/models.py
+++ museum_app/models.py
@@ -2,11 +2,9 @@ from django.db import models
 from django.db.models import Model
 from datetime import datetime, date, timedelta
 
-#help_text=
-
 
 class Room(Model):
-    name = models.CharField('Room', max_length=100)
+    name = models.CharField('Room', max_length=100, unique=True)
 
     class Meta:
         verbose_name = "Room"
@@ -18,16 +16,18 @@ class Room(Model):
 
 
 class Artwork(Model):
-    name = models.CharField('Name', max_length=100)
+    name = models.CharField('Name', max_length=100, unique=True)
     author = models.CharField('Author', max_length=100)
     period = models.CharField('Period', max_length=100)
     economic_valuation = models.PositiveIntegerField('Economic Valuation')
-    creation_date = models.DateField('Creation Date')
-    entry_date = models.DateField('Entry date')
+    creation_date = models.PositiveIntegerField('Creation Date', help_text='creation year')
+    entry_date = models.DateField('Entry date', auto_now_add=True)
     image = models.ImageField('Image', upload_to="")
     room = models.ForeignKey(Room, verbose_name='Room',
-                            on_delete=models.DO_NOTHING,
-                            related_name='room')
+                            on_delete=models.PROTECT,
+                            related_name='room',
+                            blank=True,
+                            null=True)
 
     class Meta:
         verbose_name = "Artwork"
@@ -38,28 +38,29 @@ class Artwork(Model):
         return self.name
 
     def save(self, *args, **kargs):
+        artwork = Artwork.objects.all().filter(id=self.id)
         super().save(*args, **kargs)
-        new = Exhibition(artwork=self, date_time=datetime.today())
-        new.save()
+        if len(artwork) == 0:   
+            new = Exhibition(artwork=self, date_time=datetime.today())
+            new.save()
 
 
 class State(Model):
     artwork = models.ForeignKey(Artwork, verbose_name='Artwork',
                                 on_delete=models.CASCADE,
                                 related_name='artwork')
-    date_time = models.DateTimeField('Date and Time', auto_now_add=True)
+    date_time = models.DateTimeField('State Date and Time', auto_now_add=True)
 
     class Meta:
         verbose_name = "State"
         verbose_name_plural = "States"
-        get_latest_by = "date_time"
         ordering = ["-date_time"]
 
 
 #Artwork descendants
 class Painting(Artwork):
-    technique = models.CharField('Technique', max_length=100)
-    style = models.CharField('Style', max_length=100)
+    technique = models.CharField('Technique', max_length=100, blank=True, null=True)
+    style = models.CharField('Style', max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = "Painting"
@@ -67,8 +68,8 @@ class Painting(Artwork):
         ordering = ["name"]
 
 class Sculpture(Artwork):
-    material = models.CharField('Material', max_length=100)
-    style = models.CharField('Style', max_length=100)
+    material = models.CharField('Material', max_length=100, blank=True, null=True)
+    style = models.CharField('Style', max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = "Sculpture"
@@ -77,7 +78,7 @@ class Sculpture(Artwork):
 
 
 class CollaboratingMuseum(Model):
-    name = models.CharField('Name', max_length=100)
+    name = models.CharField('Name', max_length=100, unique=True)
 
     class Meta:
         verbose_name = "Collaborating Museum"
@@ -91,11 +92,11 @@ class CollaboratingMuseum(Model):
 #State descendants
 class Loan(State):
     collaborating_museum = models.ForeignKey(CollaboratingMuseum,
-                                            on_delete=models.DO_NOTHING,
-                                            related_name='collaborating_museum')
-    loan_time = models.IntegerField('Loan time')
-    amount_received = models.IntegerField('Amount Received')
-    loan_init = models.DateField('Loan start', blank=True, null=True, editable=False)
+                                            on_delete=models.PROTECT,
+                                            related_name='Collaborating_museum')
+    loan_time = models.PositiveIntegerField('Loan Time', help_text='Loan duration in days')
+    amount_received = models.PositiveIntegerField('Amount Received')
+    loan_init = models.DateField('Loan Start Date', blank=True, null=True, editable=False)
 
     class Meta:
         verbose_name = "Loan"
@@ -113,7 +114,8 @@ class Restoration(State):
                                         blank=True)
     finish_date = models.DateField('Finish Date',
                                     blank=True,
-                                    null=True)
+                                    null=True,
+                                    editable=False)
 
     class Meta:
         verbose_name = "Restoration"
